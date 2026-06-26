a80500505593503d583e3bf3141ae189530b0e59
Nikyou <nikyou120@gmail.com>
2026-06-26 21:44:36 +0300

    Rushing Winds npc id fix

--- InstancesData.lua
+++ InstancesData.lua
@@ -549,7 +549,7 @@ MythicPlusUtility.instancesData = {
             text = format(L["{spell:%d} is cast by {npc:%d}."], 1254690, 79093),
             tags = "[creature_slow]",
         }, { -- Rushing Winds
-            text = format(L["{spell:%d} buff on {npc:%d}."], 1254670, 78096),
+            text = format(L["{spell:%d} buff on {npc:%d}."], 1254670, 76205),
             tags = "[purge][purge_spellsteal]",
         }, { -- Solar Barrier
             text = format(L["{spell:%d} buff is cast by {npc:%d}."], 1273356, 79462),
